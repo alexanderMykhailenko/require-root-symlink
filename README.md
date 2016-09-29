0e787da1564c6ac1f59d17afec5ade08d5c841f1
Alexander Mikhailenko <alexanderm@smartinfosys.com>
2016-09-29 17:18:06 +0300

    init npm package

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # require-root-symlink
 Making components first-class for node.js with symlinks
+
+INSTALL
+-------
+`npm install require-root-symlink` 
+
+USAGE
+-----
+ - In `index.js` on top add next code: `require('require-root-symlink')(__dirname);;`
+ - When you will run your nodejs application `require-root-symlink` module will check and create symlink: `node_modules/_` to `.`
+ - Now you can use `_` in `require` string:
+    
+    
+    var Config = require('_/Config.js');
+    var App = require('_/engine/App.js');
+    ..............................
+
+CHANGELOG
+---------
+### September 29, 2016
+- first release  
---
+++ index.js
@@ -0,0 +1,46 @@
+/*
+ * (C) 2016 Alexander Mykhailenko
+ * MIT LICENCE
+ */
+
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var cp = require('child_process');
+
+function init(app_root_folder) {
+    console.log('require-root-symlink start working');
+
+    var isWin = /^win/.test(process.platform);
+    var node_modules_folder = process.env.NODE_PATH ? process.env.NODE_PATH : path.resolve(app_root_folder, './node_modules');
+
+    // Check and create symlink for firt start
+    if (fs.existsSync(path.resolve(node_modules_folder, '_'))) {
+        console.log('Symlink "_" currently exist, continue working...');
+    } else {
+        if (!fs.existsSync(node_modules_folder)) {
+            console.log('Folder "' + node_modules_folder + '" NOT exist, trying make it...');
+            fs.mkdirSync(node_modules_folder);
+        }
+        console.log('Symlink "_" NOT exist, trying make it...');
+
+        var spawnArgs = {env: process.env, cwd: node_modules_folder, stdio: 'inherit'};
+        if (isWin) {
+            cp.spawnSync('cmd', ['/c', 'mklink', '/D', '_', app_root_folder], spawnArgs);
+        } else {
+            cp.spawnSync('ln', ['-s', app_root_folder, '_'], spawnArgs);
+        }
+
+        if (fs.existsSync(path.resolve(node_modules_folder, '_'))) {
+            console.log('Symlink "_" successfully created, continue working...');
+        } else {
+            console.error('Symlink "_" NOT created, try resolve it manually');
+            process.exit();
+        }
+    }
+    console.log('require-root-symlink end working');
+
+}
+
+module.exports = init;
